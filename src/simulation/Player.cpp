e6e893561eb8378326c16c5ed6438c189666be25
Elle <ellegray00@gmail.com>
2022-02-04 01:52:48 +0000

    Removed gravity

--- src/simulation/Player.cpp
+++ src/simulation/Player.cpp
@@ -9,21 +9,14 @@ class Player
     public:
         Vector2 position = {1000, 1000};
         Color color = GREEN;
-        //int velocity = 0;
         void UpdatePlayer(float delta)
-        {
+        {   
 
-            const float grav = 0.60f;       
-//            static bool isGrounded = false;
-
-            //if (player not colliding with platform) then:
-            this -> position.y += grav;     //Player will constantly move down until colliding with a platform
- 
             if (IsKeyDown(KEY_LEFT)) this->position.x -= PLAYER_SPD*delta;
             if (IsKeyDown(KEY_RIGHT)) this ->position.x += PLAYER_SPD*delta;
             if (IsKeyDown(KEY_UP)) this ->position.y -= PLAYER_SPD*delta;
-            //if (IsKeyDown(KEY_DOWN)) this-> position.y += PLAYER_SPD*delta;
+            if (IsKeyDown(KEY_DOWN)) this-> position.y += PLAYER_SPD*delta;
             Rectangle playerRect = { position.x - 20, position.y - 40, 40 ,40};
             DrawRectangleRec(playerRect, color);
-    }
+        };
 };
