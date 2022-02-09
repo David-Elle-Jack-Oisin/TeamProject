cc5e6f876b99afe4747b7a4b9b6f302f93e04144
Elle <ellegray00@gmail.com>
2022-02-09 19:51:14 +0000

    Added comments to Player.cpp

--- src/simulation/Player.cpp
+++ src/simulation/Player.cpp
@@ -1,53 +1,62 @@
 #include <iostream>
 #include <stdio.h>
 #include "raylib.h"
-#define MAX_FRAME_SPEED 15
-#define MIN_FRAME_SPEED 1
+
 
 class Player 
 {
     private:
         #define PLAYER_SPD 100.0f
+
     public:
-        int currentFrame = 0;
-    
+
     //This makes the walking animation possible
         int framesSpeed = 8;
         int framesCounter = 0;
+        int currentFrame = 0;
 
         Vector2 position = {1000, 1000};
         void UpdatePlayer(float delta)
         {   
-            Texture2D Jerry = LoadTexture("JerryWalking.png");
-            Rectangle frameRec = { 0.0f, 0.0f, (float)Jerry.width/7, (float)Jerry.height/2 };
-            DrawTextureRec(Jerry, frameRec, this-> position, WHITE);        
+
+            Texture2D Jerry = LoadTexture("JerryXmovement.png");    //Sprite Sheet (14 Sprites)
+            Rectangle frameRec = { 0.0f, 0.0f, (float)Jerry.width/14, (float)Jerry.height}; //frameRec is Jerry
+            DrawTextureRec(Jerry, frameRec, this-> position, WHITE);    //Draw Jerry
+
+            framesCounter++;
+
+            //Player Movement
 
             if (IsKeyDown(KEY_LEFT)) {
                 this->position.x -= PLAYER_SPD*delta;
+
                 if (framesCounter >= (60/framesSpeed))
                     {
                         framesCounter = 0;
-                        currentFrame++;
+                        currentFrame++;     //currentFrame is the current image of Jerry from the spritesheet
+                                            //0-7 is right-facing, 7-14 is left-facing
 
-                        if (currentFrame > 7) currentFrame = 0;
+                        if (currentFrame > 14) {currentFrame = 7;}
 
                         //This hides the other frames in the animation
                         //So that the walking looks natural
 
-                        frameRec.x = (float)currentFrame*(float)Jerry.width/5;
+                        frameRec.x = (float)currentFrame*(float)Jerry.width/14;
             } }
             if (IsKeyDown(KEY_RIGHT)) { 
                 this->position.x += PLAYER_SPD*delta;
+                
                 if (framesCounter >= (60/framesSpeed))
                     {
                         framesCounter = 0;
                         currentFrame++;
-                        if (currentFrame > 1) currentFrame = 0; //NEED to change the numbers to apply to jerry spritesheet
+
+                        if (currentFrame > 7) {currentFrame = 0; }
 
                             //This hides the other frames in the animation
                             //So that the walking looks natural
 
-                            frameRec.x = (float)currentFrame*(float)Jerry.width/5;
+                            frameRec.x = (float)currentFrame*(float)Jerry.width/14;
                         }
             }
             if (IsKeyDown(KEY_UP)) {
@@ -56,12 +65,13 @@ class Player
                     {
                         framesCounter = 0;
                         currentFrame++;
-                        if (currentFrame > 1) currentFrame = 0; //NEED to change the numbers to apply to jerry spritesheet
+
+                        if (currentFrame > 7) {currentFrame = 0;}
 
                             //This hides the other frames in the animation
                             //So that the walking looks natural
 
-                            frameRec.x = (float)currentFrame*(float)Jerry.width/5;
+                            frameRec.x = (float)currentFrame*(float)Jerry.width/14;
                         }
             }
             if (IsKeyDown(KEY_DOWN)) {
@@ -70,13 +80,15 @@ class Player
                     {
                         framesCounter = 0;
                         currentFrame++;
-                        if (currentFrame > 1) currentFrame = 0; //NEED to change the numbers to apply to jerry spritesheet
+
+                        if (currentFrame > 14) {currentFrame = 7;}
 
                             //This hides the other frames in the animation
                             //So that the walking looks natural
 
-                            frameRec.x = (float)currentFrame*(float)Jerry.width/5;
+                            frameRec.x = (float)currentFrame*(float)Jerry.width/14;
                         }
             }
+            
             };
         };
