efd4f596175cacea52c4621fc25a0c231828858a
notreallyelle <ellegray00@gmail.com>
2022-02-28 00:15:15 +0000

    Added bullet class

---
+++ src/simulation/Bullet.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdio.h>
+#include "raylib.h"
+
+class Bullet {
+    private:
+    Vector2 position;
+    Vector2 speed;
+    float radius;
+    float rotation;
+    int lifeSpan;
+    int shotDamage = 1;
+    bool active;
+
+    public:
+
+        if (IsKeyPressed(KEY_SPACE)){
+            for (int i = 0; i < 10; i++)
+                {
+                    if (!shoot[i].active)
+                    {
+                        shoot[i].position = (Vector2) player->position.x , player->position.y
+                        shoot[i].active = true;
+                        shoot[i].speed.x = 
+                        shoot[i].speed.y = 
+                        break;
+                    }
+                }
+            }
+};
