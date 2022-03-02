cebf182daf31590c69e01ed99bd0a81ab723f33e
HeraclitusDEF <davebodruzzaman@gmail.com>
2022-03-02 15:13:35 +0000

    Hard coded rough collisions

--- src/simulation/MapGenerator.cpp
+++ src/simulation/MapGenerator.cpp
@@ -124,59 +124,147 @@ class MapGenerator {
 
 // =================================== CLASS MAP DRAW ===============================
 
-
     int ClassDrawMap(){
         startingRoom.Draw();
     }
 
-    bool collideFromLeft(Player *player){
-        return player->prevHitboxPosition.x + player->hitBox.width < startingRoom.Top.x && player->hitboxPosition.x + player->hitBox.width >= startingRoom.Top.x;  
-    }
-
-    bool collideFromRight(Player *player){
-        return player->prevHitboxPosition.x >= startingRoom.Top.x + startingRoom.Top.width && player->hitboxPosition.x < startingRoom.Top.x +startingRoom.Top.width;
-    }   
-
-    bool collideFromTop(Player *player){
-        return player->prevHitboxPosition.y + player->hitBox.height < startingRoom.Top.y && player->hitboxPosition.y + player->hitBox.height >= startingRoom.Top.y;  
-    }
-
-    bool collideFromBottom(Player *player){
-        return player->prevHitboxPosition.y  >= startingRoom.Top.y + startingRoom.Top.height && player->hitboxPosition.y < startingRoom.Top.y + startingRoom.Top.height;  
-    }
+// ==================================== Directional Collisions ===========================
 
 
-    int CheckCollision(Player *player){
+   // bool collideFromBottom(Player *player){
+    //     return player->prevHitboxPosition.y  >= startingRoom.Top.y + startingRoom.Top.height && player->hitboxPosition.y 
+    //     <= startingRoom.Top.y + startingRoom.Top.height;  
+    // }
+         // I think prevHitboxPosition and hitboxPosition are not properly being reset to pre collision states
+
+
+            // If hit from bottom
+
+
+
+    // bool collideFromLeft(Player *player){
+    //     return player->prevHitboxPosition.x + player->hitBox.width < startingRoom.Top.x && player->hitboxPosition.x + player->hitBox.width >= startingRoom.Top.x;  
+    // }
+
+    // bool collideFromRight(Player *player){
+    //     return player->prevHitboxPosition.x >= startingRoom.Top.x + startingRoom.Top.width && player->hitboxPosition.x < startingRoom.Top.x +startingRoom.Top.width;
+    // }   
+
+    // bool collideFromTop(Player *player){
+    //     return player->prevHitboxPosition.y + player->hitBox.height < startingRoom.Top.y && player->hitboxPosition.y + player->hitBox.height >= startingRoom.Top.y;  
+    // }
+
+    // bool collideFromBottom(Player *player){
+    //     return player->prevHitboxPosition.y  >= startingRoom.Top.y + startingRoom.Top.height && player->hitboxPosition.y <= startingRoom.Top.y + startingRoom.Top.height;  
+    // }
+
+    //             if(collideFromLeft(player)){
+    //             // player->position.x = startingRoom.Top.x -1;
+    //             // player->hitboxPosition.x = startingRoom.Top.x - 1;
+    //         }
+
+    //         if(collideFromRight(player)){
+    //             fprintf(stderr,"right Collision\n");
+    //         }
+
+    //         if(collideFromTop(player)){
+    //             fprintf(stderr,"Top Collision\n");
+    //             fprintf(stderr, "%i", player->hitboxPosition.x);
+    //         }
+
+    //         // fprintf(stderr, "%i\n", player->prevPosition.x);
+    //        // fprintf(stderr, "%i\n", player->prevPosition.y);
+
+            // if(collideFromBottom(player)){
+            //     fprintf(stderr,"Bottom Collision\n");
+
+
+                
+            //     //player->hitboxPosition.y = startingRoom.Top.height + startingRoom.Top.y ;
+            //     //player->prevHitboxPosition.y = startingRoom.Top.height + startingRoom.Top.y;
+
+            //     // Resolves collision
+            //    player->position.y = startingRoom.Top.height + startingRoom.Top.y + 1;
+            //    player->prevHitboxPosition.y = player->hitboxPosition.y;
+            //    player->hitboxPosition.y = startingRoom.Top.y + startingRoom.Top.height + 1;
+                
+
+            // }
+
+//   =========================================================================================================
+
+    int CheckCollisionTop(Player *player){
 
         if((CheckCollisionRecs(player->hitBox, startingRoom.Top))){
             //fprintf(stderr,"Collision\n");
             DrawRectangleRec(player->frameRec, RED);
             DrawRectangleRec(player->hitBox, GREEN);
 
-            if(collideFromLeft(player)){
-                fprintf(stderr,"left Collision\n");
-            }
-
-            if(collideFromRight(player)){
-                fprintf(stderr,"right Collision\n");
-            }
-
-            if(collideFromTop(player)){
-                fprintf(stderr,"Top Collision\n");
-            }
-
-            if(collideFromBottom(player)){
-                fprintf(stderr,"Bottom Collision\n");
-            }
-            // If hit from bottom
-            // if(player->hitBox.y < (startingRoom.Top.height + startingRoom.Top.y) && player ->hitBox.x  > startingRoom.Top.x && player ->hitBox.x < (startingRoom.Top.x + startingRoom.Top.width)){
+            if(player->hitBox.y < (startingRoom.Top.height + startingRoom.Top.y) && player ->hitBox.x  > startingRoom.Top.x && player ->hitBox.x < (startingRoom.Top.x + startingRoom.Top.width)){
                 
-            //     player->position.y = startingRoom.Top.height + startingRoom.Top.y;
-            //     fprintf(stderr,"Up Collision\n");
-                
-
-            // }
+                player->position.y = startingRoom.Top.height + startingRoom.Top.y;
+                fprintf(stderr,"Up Collision\n");
+            }
         }
+    }
+
+    int CheckCollisionBottom(Player *player){
+
+        if((CheckCollisionRecs(player->hitBox, startingRoom.Bottom))){
+            //fprintf(stderr,"Collision\n");
+            DrawRectangleRec(player->frameRec, RED);
+            DrawRectangleRec(player->hitBox, GREEN);
+
+            // && player ->hitBox.x  > startingRoom.Bottom.x && player ->hitBox.x < (startingRoom.Bottom.x + startingRoom.Top.width
+
+            if((player->hitBox.y + player->hitBox.height) > (startingRoom.Bottom.y)){
+                fprintf(stderr, "Bottom Collision");
+                player->position.y  = (startingRoom.Bottom.y - player->hitBox.height - 11);
+               // fprintf(stderr,"Up Collision\n");
+            }
+        }
+    }
+
+    int CheckCollisionLeft(Player *player){
+
+        if((CheckCollisionRecs(player->hitBox, startingRoom.LSide))){
+            //fprintf(stderr,"Collision\n");
+            DrawRectangleRec(player->frameRec, RED);
+            DrawRectangleRec(player->hitBox, GREEN);
+
+            // && player ->hitBox.x  > startingRoom.Bottom.x && player ->hitBox.x < (startingRoom.Bottom.x + startingRoom.Top.width
+
+            if((player->hitBox.x  < (startingRoom.LSide.x + startingRoom.LSide.width))){
+                fprintf(stderr, "Left Collision");
+                player->position.x  = (startingRoom.LSide.x + startingRoom.LSide.width);
+               // fprintf(stderr,"Up Collision\n");
+            }
+        }
+    }
+
+
+    int CheckCollisionRight(Player *player){
+
+        if((CheckCollisionRecs(player->hitBox, startingRoom.RSide))){
+            //fprintf(stderr,"Collision\n");
+            DrawRectangleRec(player->frameRec, RED);
+            DrawRectangleRec(player->hitBox, GREEN);
+
+            // && player ->hitBox.x  > startingRoom.Bottom.x && player ->hitBox.x < (startingRoom.Bottom.x + startingRoom.Top.width
+
+            if(((player->hitBox.x + player->hitBox.width) > startingRoom.RSide.x)){
+                fprintf(stderr, "Left Collision");
+                player->position.x  = (startingRoom.RSide.x - startingRoom.RSide.width);
+               // fprintf(stderr,"Up Collision\n");
+            }
+        }
+    }
+
+    int CheckCollision(Player *player){
+        CheckCollisionTop(player);
+        CheckCollisionBottom(player);
+        CheckCollisionLeft(player);
+        CheckCollisionRight(player);
     }
 
 };
